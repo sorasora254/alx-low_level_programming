7f807c6059850ccff18292b12e5ca7a5d79bb194
sorasora254 <sorasoratech@gmail.com>
2023-10-06 15:16:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:
+ * Return: zero
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 122)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
