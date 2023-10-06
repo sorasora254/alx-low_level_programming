81ef5a0a7d69bf7348513b92f327482d596a3b51
sorasora254 <sorasoratech@gmail.com>
2023-10-06 13:58:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:
+ * Return: zero success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
