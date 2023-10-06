a234e7af5dadf7df02d864b02886937992334dc2
sorasora254 <sorasoratech@gmail.com>
2023-10-06 15:35:19 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: hexadecima	l
+ * Return: zero
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (m = 97; m <= 102; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
