bad191f5d533252cbbb66bdc2ffd4812f16110b6
sorasora254 <sorasoratech@gmail.com>
2023-10-11 05:57:17 -0700

    function and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main = entry point where code will start its execution
+ * Description: function and nested loops
+ * Return: always retunr zero
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
