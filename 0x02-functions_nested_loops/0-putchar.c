92351226f1e06fd79af495a846c436944a450f69
sorasora254 <sorasoratech@gmail.com>
2023-10-24 01:03:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main = entry point for code execution
+ * Descripton: use of putchar
+ * Return: success zero
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
